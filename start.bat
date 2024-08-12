c5f295fd402f792b132fb4e8f287439d35e67f43
oboard <oboard@outlook.com>
2024-08-13 01:57:20 +0800

    lib move to /src, upgrade chalk, mimetype, upgrade moonbit

--- start.bat
+++ start.bat
@@ -6,7 +6,7 @@ REM Create the target directory for the application
 mkdir ./target/app/node
 
 REM Copy the entire node directory to the target directory
-xcopy /E /I ./node ./target/app/node
+xcopy /E /I ./src/node ./target/app/node
 
 REM Copy the compiled WebAssembly file to the target directory
 copy ./target/wasm/release/build/main/main.wasm ./target/app/node/assets/app.wasm
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 moon build --target=wasm
 mkdir -p ./target/app/node
-cp -rf ./node/ ./target/app/node/
+cp -rf ./src/node/ ./target/app/node/
 cp ./target/wasm/release/build/main/main.wasm ./target/app/node/assets/app.wasm
 cd ./target/app/node/
 npm install
